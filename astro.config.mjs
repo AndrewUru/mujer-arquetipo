8590a6273ff2cee4c9f207fc5dfb25c72261cb61
Andres Tobio <92232669+AndrewUru@users.noreply.github.com>
2025-04-17 01:15:14 +0200

    Add @astrojs/vercel dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless"; // 👈 esto es lo importante
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  integrations: [react()], // 👈 aquí activamos React
+  integrations: [react()],
+  adapter: vercel(), // 👈 aquí se activa el adaptador
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
   },
 });
