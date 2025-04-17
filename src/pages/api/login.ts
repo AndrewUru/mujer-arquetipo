da28493c55a1c71895231ff70072739d7d895829
Andres Tobio <92232669+AndrewUru@users.noreply.github.com>
2025-04-17 23:42:29 +0200

    fix: Actualizar las importaciones de supabase en Navbar, login y dashboard, y restaurar el archivo supabaseClient.ts

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -2,7 +2,7 @@
 export const prerender = false;
 
 import type { APIRoute } from "astro";
-import { supabase } from "../../scripts/supabaseClient";
+import { supabase } from "../../lib/supabaseClient";
 
 export const POST: APIRoute = async ({ request, redirect }) => {
   const formData = await request.formData();
