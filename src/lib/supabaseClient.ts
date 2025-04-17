61b0f639e19aa954d94350c2e1063ec3d69d163c
Andres Tobio <92232669+AndrewUru@users.noreply.github.com>
2025-04-17 23:22:20 +0200

    fix: Actualizar las importaciones de supabase en supabaseClient.ts, dashboard.astro y setup.astro

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,9 @@
-// src/lib/supabaseClient.ts
-import { createClient } from "@supabase/supabase-js";
+// src/scripts/supabaseClient.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+const supabase = createClient(
+  import.meta.env.PUBLIC_SUPABASE_URL,
+  import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+);
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export { supabase };
